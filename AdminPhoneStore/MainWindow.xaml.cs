2cff160c9d8778f532560805f4fb492d019b61b9
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2026-01-30 11:01:30 +0700

    Add project files.

---
+++ AdminPhoneStore/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using AdminPhoneStore.Views;
+using System.Windows;
+
+namespace AdminPhoneStore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainContent.Content = new DashboardView();
+        }
+    }
+}
---
+++ AdminPhoneStore/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AdminPhoneStore.ViewModels
+{
+    class DashboardViewModel : BaseViewModel
+    {
+        private string _title = "Dashboard Admin 123";
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
